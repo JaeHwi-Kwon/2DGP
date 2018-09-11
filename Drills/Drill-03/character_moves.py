443b963e10ed09f16f8e435f596704f1e030ad17
2DGP <kegedokoota@naver.com>
2018-09-11 16:36:06 +0900

    Drill 04 완성
    2014180003 권재휘
    https://youtu.be/eCL91Q7Fehs
    
    +Drill 04 added.
      - character moves left and right with some of simple animations.
    +Solve Drill 03's flickering problem.
      - clear_canvas_now() -> clear_canvas()
      - draw_now() -> draw()
      - add update_canvas()
      - add get_events()

--- Drills/Drill-03/character_moves.py
+++ Drills/Drill-03/character_moves.py
@@ -8,50 +8,62 @@ character=load_image('character.png')
 def rect_mov():
     x=400
     while x<800:
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
+        clear_canvas()
+        grass.draw(400,30)
+        character.draw(x,90)
+        update_canvas()
         x=x+2
         delay(0.005)
+        get_events()
 
     y=90
     while y<600:
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
+        clear_canvas()
+        grass.draw(400,30)
+        character.draw(x,y)
+        update_canvas()
         y=y+2
         delay(0.005)
+        get_events()
     
     while x>0:
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
+        clear_canvas()
+        grass.draw(400,30)
+        character.draw(x,y)
+        update_canvas()
         x=x-2
         delay(0.005)
+        get_events()
 
     while y>90:
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
+        clear_canvas()
+        grass.draw(400,30)
+        character.draw(x,y)
+        update_canvas()
         y=y-2
         delay(0.005)
+        get_events()
 
     while x<400:
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
+        clear_canvas()
+        grass.draw(400,30)
+        character.draw(x,90)
+        update_canvas()
         x=x+2
         delay(0.005)
+        get_events()
 
 def circle_mov():
     pi=3.14
     r=pi*3/2
     while r<pi*7/2:
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(210*(math.cos(r))-0*(math.sin(r))+400,210*(math.sin(r))+0*(math.cos(r))+300)
+        clear_canvas()
+        grass.draw(400,30)
+        character.draw(210*(math.cos(r))-0*(math.sin(r))+400,210*(math.sin(r))+0*(math.cos(r))+300)
+        update_canvas()
         r=r+pi/180
         delay(0.01)
+        get_events()
 
 while True:
     rect_mov()
