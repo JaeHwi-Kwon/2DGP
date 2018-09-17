57bc221004046894555af878e47fefe47acfe6e5
W_Macaron(EDU_C221) <kegedokoota@naver.com>
2018-09-17 15:02:53 +0900

    STEP 02 완성
    2014180003 권재휘
    
    + add return 2 values in set_initial_pos()
    + add x,y values in main while loop
    + set x,y initial position value with set_initial_pos()

--- Drills/Drill-05/character_moves.py
+++ Drills/Drill-05/character_moves.py
@@ -4,13 +4,13 @@ grass=load_image('grass.png')
 character=load_image('animation_sheet.png')
 
 def set_initial_pos():
-    pass
+    return 203,535
 def mov_character_by_route():
     pass
 
 
 while True:
-    set_initial_pos()
+    x,y=set_initial_pos()
     mov_character_by_route()
 
-#close_canvas()
+close_canvas()
