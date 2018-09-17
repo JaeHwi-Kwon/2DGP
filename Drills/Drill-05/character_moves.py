7011cad621a4994a54b5a8650239a9c61c6974d1
2DGP <kegedokoota@naver.com>
2018-09-17 20:10:03 +0900

    STEP 06 완성
    2014180003 권재휘
    
    + delete set_initial_pos()
    + add mov_up()
    + add mov_down()
    + delete destination value in mov_character()
    + delete the function about setting destination value
    + change all x, y values in mov_character_from_to() to actual position information

--- Drills/Drill-05/character_moves.py
+++ Drills/Drill-05/character_moves.py
@@ -1,22 +1,18 @@
 from pico2d import*
 open_canvas()
-grass=load_image('grass.png')
-character=load_image('animation_sheet.png')
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
 
-def set_initial_pos():
-    return 203,535
+def mov_character(x, y, desx, desy):
 
-def mov_character(x,y,desx,desy,des):
-
-    def mov_left(x,y,desx,desy):
+    def mov_left(x, y, desx, desy):
         pass
-    def mov_right(x,y,desx,desy):
+    def mov_right(x, y, desx, desy):
         pass
-
-    if des=='left':
-        mov_left(x,y,desx,desy)
-    elif des=='right':
-        mov_right(x,y,desx,desy)
+    def mov_up(x, y, desx,desy):
+        pass
+    def mov_down(x, y, desx, desy):
+        pass
 
 
 def mov_character_by_route():
@@ -30,23 +26,22 @@ def mov_character_by_route():
                 return 'left'
 
         destination = set_anime_destination(x, y, desx, desy)
-        mov_character(x, y, desx, desy,destination)
+        mov_character(x, y, desx, desy)
 
-    mov_character_from_to(x, y, 132, 243)
-    mov_character_from_to(x, y, 535, 470)
-    mov_character_from_to(x, y, 477, 203)
-    mov_character_from_to(x, y, 715, 136)
-    mov_character_from_to(x, y, 316, 225)
-    mov_character_from_to(x, y, 510, 92)
-    mov_character_from_to(x, y, 692, 518)
-    mov_character_from_to(x, y, 682, 336)
-    mov_character_from_to(x, y, 712, 349)
-    mov_character_from_to(x, y, 203, 535)
+    mov_character_from_to(203, 535, 132, 243)
+    mov_character_from_to(132, 243, 535, 470)
+    mov_character_from_to(535, 470, 477, 203)
+    mov_character_from_to(477, 203, 715, 136)
+    mov_character_from_to(715, 136, 316, 225)
+    mov_character_from_to(316, 225, 510, 92)
+    mov_character_from_to(510, 92, 692, 518)
+    mov_character_from_to(692, 518, 682, 336)
+    mov_character_from_to(682, 336, 712, 349)
+    mov_character_from_to(712, 349, 203, 535)
     pass
 
 
 while True:
-    x,y=set_initial_pos()
     mov_character_by_route()
 
 close_canvas()
