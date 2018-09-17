d4ade80c34ba1f7f32a20ce5d0b30342a577beb5
2DGP <kegedokoota@naver.com>
2018-09-17 20:41:10 +0900

    STEP 08 완성
    2014180003 권재휘
    
    + defined mov_left

--- Drills/Drill-05/character_moves.py
+++ Drills/Drill-05/character_moves.py
@@ -5,60 +5,53 @@ character = load_image('animation_sheet.png')
 
 def mov_character(x, y, desx, desy):
 
-    def mov_left(x, y, desx, desy):
+    def mov_left(x):
+        return x - 1
+    def mov_right(x):
         pass
-    def mov_right(x, y, desx, desy):
+    def mov_up(y):
         pass
-    def mov_up(x, y, desx,desy):
-        pass
-    def mov_down(x, y, desx, desy):
+    def mov_down(y):
         pass
 
     frame = 0
     destination = 0
-    while x != desx and y != desy:
+    while x != desx or y != desy:
         if x > desx:
-            mov_left(x,y,desx,desy)
+            x = mov_left(x)
             destination = 0
         elif x < desx:
-            mov_right(x,y,desx,desy)
+            x = mov_right(x)
             destination = 100
+
         if y > desy:
-            mov_down(x,y,desx,desy)
+            y = mov_down(y)
         elif y < desy:
-            mov_up(x,y,desx,desy)
+            y = mov_up(y)
         clear_canvas()
         grass.draw(400,30)
         character.clip_draw(frame*100,destination,100,100,x,y)
         update_canvas()
         frame = (frame + 1) % 8
-        delay(0.05)
+        delay(0.02)
         get_events()
 
 
 def mov_character_by_route():
 
     def mov_character_from_to(x, y, desx, desy):
-
-        def set_anime_destination(x, y, desx, desy):
-            if x < desx:
-                return 'right'
-            elif x > desx:
-                return 'left'
-
-        destination = set_anime_destination(x, y, desx, desy)
         mov_character(x, y, desx, desy)
 
     mov_character_from_to(203, 535, 132, 243)
-    mov_character_from_to(132, 243, 535, 470)
-    mov_character_from_to(535, 470, 477, 203)
-    mov_character_from_to(477, 203, 715, 136)
-    mov_character_from_to(715, 136, 316, 225)
-    mov_character_from_to(316, 225, 510, 92)
-    mov_character_from_to(510, 92, 692, 518)
-    mov_character_from_to(692, 518, 682, 336)
-    mov_character_from_to(682, 336, 712, 349)
-    mov_character_from_to(712, 349, 203, 535)
+    #mov_character_from_to(132, 243, 535, 470)
+    #mov_character_from_to(535, 470, 477, 203)
+    #mov_character_from_to(477, 203, 715, 136)
+    #mov_character_from_to(715, 136, 316, 225)
+    #mov_character_from_to(316, 225, 510, 92)
+    #mov_character_from_to(510, 92, 692, 518)
+    #mov_character_from_to(692, 518, 682, 336)
+    #mov_character_from_to(682, 336, 712, 349)
+    #mov_character_from_to(712, 349, 203, 535)
     pass
 
 
