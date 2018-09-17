b2bc5be682f9f001989c08ddff985966f47540d8
W_Macaron(EDU_C221) <kegedokoota@naver.com>
2018-09-17 15:25:15 +0900

    STEP 05 완성
    2014180003 권재휘
    
    +define mov_character()
    + add mov_left()
    + add mov_right()
    +add destination value to mov_character()
    +move set_anime_destination() to mov_character_from_to()

--- Drills/Drill-05/character_moves.py
+++ Drills/Drill-05/character_moves.py
@@ -6,15 +6,31 @@ character=load_image('animation_sheet.png')
 def set_initial_pos():
     return 203,535
 
-def set_anime_destination(x,y,desx,desy):
-    pass
-def mov_character(x,y,desx,desy):
-    pass
+def mov_character(x,y,desx,desy,des):
+
+    def mov_left(x,y,desx,desy):
+        pass
+    def mov_right(x,y,desx,desy):
+        pass
+
+    if des=='left':
+        mov_left(x,y,desx,desy)
+    elif des=='right':
+        mov_right(x,y,desx,desy)
+
 
 def mov_character_by_route():
+
     def mov_character_from_to(x, y, desx, desy):
+
+        def set_anime_destination(x, y, desx, desy):
+            if x < desx:
+                return 'right'
+            elif x > desx:
+                return 'left'
+
         destination = set_anime_destination(x, y, desx, desy)
-        mov_character(x, y, desx, desy)
+        mov_character(x, y, desx, desy,destination)
 
     mov_character_from_to(x, y, 132, 243)
     mov_character_from_to(x, y, 535, 470)
