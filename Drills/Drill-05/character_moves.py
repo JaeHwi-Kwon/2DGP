3d31d43e55bdc32458f79358a722c42e693ab555
W_Macaron(EDU_C221) <kegedokoota@naver.com>
2018-09-17 14:59:25 +0900

    STEP 01  완성
    2014180003 권재휘
    + set_initial_pos()
    + mov_character_by_route()

---
+++ Drills/Drill-05/character_moves.py
@@ -0,0 +1,16 @@
+from pico2d import*
+open_canvas()
+grass=load_image('grass.png')
+character=load_image('animation_sheet.png')
+
+def set_initial_pos():
+    pass
+def mov_character_by_route():
+    pass
+
+
+while True:
+    set_initial_pos()
+    mov_character_by_route()
+
+#close_canvas()
