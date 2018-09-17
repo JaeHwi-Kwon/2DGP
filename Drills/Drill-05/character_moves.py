d19063c571e8bac49220236a83e634dc58b9c25e
W_Macaron(EDU_C221) <kegedokoota@naver.com>
2018-09-17 15:09:17 +0900

    STEP 03 완성
    2014180003 권재휘
    
    +add mov_character_from_to() in mov_character_by_route()
    
    +set 10 destinations in mov_character_by_route()

--- Drills/Drill-05/character_moves.py
+++ Drills/Drill-05/character_moves.py
@@ -5,7 +5,21 @@ character=load_image('animation_sheet.png')
 
 def set_initial_pos():
     return 203,535
+
+def mov_character_from_to(x,y,desx,desy):
+    pass
+
 def mov_character_by_route():
+    mov_character_from_to(x, y, 132, 243)
+    mov_character_from_to(x, y, 535, 470)
+    mov_character_from_to(x, y, 477, 203)
+    mov_character_from_to(x, y, 715, 136)
+    mov_character_from_to(x, y, 316, 225)
+    mov_character_from_to(x, y, 510, 92)
+    mov_character_from_to(x, y, 692, 518)
+    mov_character_from_to(x, y, 682, 336)
+    mov_character_from_to(x, y, 712, 349)
+    mov_character_from_to(x, y, 203, 535)
     pass
 
 
