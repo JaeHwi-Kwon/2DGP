244ef2ab3e902d91f8a4aa74b921dff2d8449bff
kdgkt123 <kegedokoota@naver.com>
2018-10-02 16:38:30 +0900

    Drill 07-01
    add basic

---
+++ Drills/Drill-07/NPC_movement.py
@@ -0,0 +1,15 @@
+from pico2d import*
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+
+
+
+
+open_canvas()
+
+while True:
+    pass
+
+close_canvas()
