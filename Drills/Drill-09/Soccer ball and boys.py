ea7c3ff86d4aec92b02a251355aef8d619d548cd
2DGP <kegedokoota@naver.com>
2018-10-09 14:53:58 +0900

    Drill 09 STEP 12
    modify typo
     ball21xz21 -> ball21x21
     randing -> randint
    
    add draw ball and boys on canvas code

--- Drills/Drill-09/Soccer ball and boys.py
+++ Drills/Drill-09/Soccer ball and boys.py
@@ -24,12 +24,18 @@ class BigBall:
         self.image = load_image('ball41x41.png')
         self.velocity = random.randint(1, 8)
 
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
 
 class SmallBall:
     def __init__(self):
-        self.x, self.y = random.randing(10, 790), random.randint(310, 590)
-        self.image = load_image('ball21xz21.png')
+        self.x, self.y = random.randint(10, 790), random.randint(310, 590)
+        self.image = load_image('ball21x21.png')
         self.velocity = random.randint(1, 8)
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
 
 
 class Grass:
@@ -37,7 +43,7 @@ class Grass:
         self.image = load_image('grass.png')
 
     def draw(self):
-        self.image.draw(400,30)
+        self.image.draw(400, 30)
 
 
 def handle_events():
@@ -52,15 +58,27 @@ def handle_events():
 
 # Initialization
 open_canvas()
+grass = Grass()
 team = [Boy() for i in range(11)]
 rand_ball = random.randint(1, 19)
-bigball = [BigBall() for i in range(rand_ball)]
-smallball = [SmallBall() for i in range(20-rand_ball)]
+B_ball = [BigBall() for i in range(rand_ball)]
+S_ball = [SmallBall() for i in range(20-rand_ball)]
 running = True
 
 # Main loop
 while running:
-    pass
+    handle_events()
+
+    clear_canvas()
+    grass.draw()
+    for boy in team:
+        boy.draw()
+    for bigball in B_ball:
+        bigball.draw()
+    for smallball in S_ball:
+        smallball.draw()
+    update_canvas()
+    delay(0.05)
 
 # Finalization
 close_canvas()
