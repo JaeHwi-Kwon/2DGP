fa0c5205fdd5746cbb994d8c1a92782c55bc4f30
2DGP <kegedokoota@naver.com>
2018-10-09 13:17:53 +0900

    Drill 09 STEP 01
    create Drill 09 folder
    copy Drill 09 resources
    create Drill 09 file (Soccer ball and boys)
    make the basement codes

---
+++ Drills/Drill-09/Soccer ball and boys.py
@@ -0,0 +1,21 @@
+from pico2d import *
+import random
+
+#Object class
+class Boy:
+    pass
+class Ball:
+    pass
+class grass:
+    pass
+
+#Initialization
+open_canvas()
+running=True
+
+#Main loop
+while running:
+    pass
+
+#Finalization
+close_canvas()
