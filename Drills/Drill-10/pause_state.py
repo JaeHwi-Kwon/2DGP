7afea41d3810f7ec953eb5c71065c4281f96ee61
2DGP <kegedokoota@naver.com>
2018-10-15 20:56:50 +0900

    Drill 10-2 STEP 02
    add new value
     button_state
     button_timer
    
    defined update() function
    set delay time 0.01 sec
    button_timer increases 0.05 every update() has called
    
    modified draw() function
    draw pause button only when the button_state is True

--- Drills/Drill-10/pause_state.py
+++ Drills/Drill-10/pause_state.py
@@ -6,6 +6,8 @@ import main_state
 
 name = 'PauseState'
 image = None
+button_state = True
+button_timer = 0.0
 
 
 def enter():
@@ -32,12 +34,22 @@ def handle_events():
             game_framework.pop_state()
 
 
-def update(): pass
+def update():
+    global button_state
+    global button_timer
+    if(button_timer > 1.0):
+        button_timer = 0
+        if button_state == True: button_state = False
+        elif button_state == False : button_state = True
+    delay(0.01)
+    button_timer += 0.05
+
 
 
 def draw():
     clear_canvas()
     main_state.boy.draw()
     main_state.grass.draw()
-    image.draw(400,300)
+    if button_state == True:
+        image.draw(400,300)
     update_canvas()
