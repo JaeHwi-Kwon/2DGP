b44e943ed452246e7aaf34c9ae1afc84131e57ac
2DGP <kegedokoota@naver.com>
2018-10-15 20:34:33 +0900

    Drill 10-1 STEP 05
    modified pause.png
     - changed scale
     - delete the background

--- Drills/Drill-10/pause_state.py
+++ Drills/Drill-10/pause_state.py
@@ -1,6 +1,8 @@
 import game_framework
 from pico2d import *
 
+import title_state
+
 name = 'PauseState'
 image = None
 
@@ -20,7 +22,14 @@ def resume():
 
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
 
 
 def update():
