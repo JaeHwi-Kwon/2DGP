4c467fef6273c8bf7a59c8c1a243f732149e87bc
2DGP <kegedokoota@naver.com>
2018-11-01 04:03:16 +0900

    2DGP Project D3
    Character Controller Fixed
    - Now the screen is moved with 60FPS
    - Deleted JumpState
    - Added UP_DOWN, UP_UP
    - Now the Jump moves ordinarily

--- 2DGP 게임 프로젝트/John.py
+++ 2DGP 게임 프로젝트/John.py
@@ -1,7 +1,23 @@
+import game_framework
 from pico2d import *
 
+import game_world
+
+# Boy Run Speed
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 40.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0/60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# Boy Action Speed
+# fill expressions correctly
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 # John Event
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, UP_DOWN, IDLE_UP, RUN_UP = range(7)
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, UP_DOWN, UP_UP = range(6)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
@@ -9,27 +25,32 @@ key_event_table = {
     (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
     (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
     (SDL_KEYDOWN, SDLK_UP): UP_DOWN,
-    (SDL_KEYUP, SDLK_UP): IDLE_UP,
-    (RIGHT_DOWN, SDL_KEYUP, SDLK_UP): RUN_UP,
-    (LEFT_DOWN, SDL_KEYUP, SDLK_UP): RUN_UP
+    (SDL_KEYUP, SDLK_UP): UP_UP
 }
 
-gravity = 0.3
+gravity = 0.03
+
 
 # John States
-
 class IdleState:
 
     @staticmethod
     def enter(John, event):
         if event == RIGHT_DOWN:
-            John.velocity += 1
+            John.velocity += RUN_SPEED_PPS
         elif event == LEFT_DOWN:
-            John.velocity -= 1
+            John.velocity -= RUN_SPEED_PPS
         elif event == RIGHT_UP:
-            John.velocity -= 1
+            John.velocity -= RUN_SPEED_PPS
         elif event == LEFT_UP:
-            John.velocity += 1
+            John.velocity += RUN_SPEED_PPS
+        elif event == UP_DOWN:
+            if John.y2 == John.y:
+                John.jump = 4.0
+        elif event == UP_UP:
+            if John.jump > 0.0:
+                John.jump = 0.0
+
         John.timer = 300
 
     @staticmethod
@@ -39,18 +60,18 @@ class IdleState:
 
     @staticmethod
     def do(John):
-        John.frame = (John.frame + 1) % 8
+        John.y2 = John.y
+        John.frame = (John.frame + FRAMES_PER_ACTION*ACTION_PER_TIME*game_framework.frame_time) % 8
         John.y += John.jump
-        John.y = clamp(90, John.y, 800)
+        John.y = clamp(90.0, John.y, 1200.0)
         John.jump -= gravity
-        # fill here
 
     @staticmethod
     def draw(John):
         if John.dir == 1:
-            John.image.clip_draw(John.frame * 100, 300, 100, 100, John.x, John.y)
+            John.image.clip_draw(int(John.frame) * 100, 300, 100, 100, John.x, John.y)
         else:
-            John.image.clip_draw(John.frame * 100, 200, 100, 100, John.x, John.y)
+            John.image.clip_draw(int(John.frame) * 100, 200, 100, 100, John.x, John.y)
 
 
 class RunState:
@@ -58,14 +79,21 @@ class RunState:
     @staticmethod
     def enter(John, event):
         if event == RIGHT_DOWN:
-            John.velocity += 1
+            John.velocity += RUN_SPEED_PPS
         elif event == LEFT_DOWN:
-            John.velocity -= 1
+            John.velocity -= RUN_SPEED_PPS
         elif event == RIGHT_UP:
-            John.velocity -= 1
+            John.velocity -= RUN_SPEED_PPS
         elif event == LEFT_UP:
-            John.velocity += 1
+            John.velocity += RUN_SPEED_PPS
+        elif event == UP_DOWN:
+            if John.y2 == John.y:
+                John.jump = 4.0
+        elif event == UP_UP:
+            if John.jump > 0.0:
+                John.jump = 0.0
         John.dir = John.velocity
+        John.dir = clamp(-1, John.velocity, 1)
 
     @staticmethod
     def exit(John, event):
@@ -73,81 +101,44 @@ class RunState:
 
     @staticmethod
     def do(John):
-        John.frame = (John.frame + 1) % 8
+        John.y2 = John.y
+        John.frame = (John.frame + FRAMES_PER_ACTION*ACTION_PER_TIME*game_framework.frame_time) % 8
         John.timer -= 1
-        John.x += John.velocity
-        John.x = clamp(25, John.x, 1600 - 25)
+        John.x += John.velocity * game_framework.frame_time
+        John.x = clamp(25, John.x, 1920 - 25)
         John.y += John.jump
-        John.y = clamp(90, John.y, 800)
+        John.y = clamp(90.0, John.y, 1200.0)
         John.jump -= gravity
 
 
     @staticmethod
     def draw(John):
-        if John.velocity == 1:
-            John.image.clip_draw(John.frame * 100, 100, 100, 100, John.x, John.y)
+        if John.dir == 1:
+            John.image.clip_draw(int(John.frame) * 100, 100, 100, 100, John.x, John.y)
         else:
-            John.image.clip_draw(John.frame * 100, 0, 100, 100, John.x, John.y)
-
-
-class JumpState:
-
-    @staticmethod
-    def enter(John, event):
-        if event == RIGHT_DOWN:
-            John.velocity += 1
-        elif event == LEFT_DOWN:
-            John.velocity -= 1
-        elif event == RIGHT_UP:
-            John.velocity -= 1
-        elif event == LEFT_UP:
-            John.velocity += 1
-        John.dir = John.velocity
-        John.jump = 10
-        pass
-
-    @staticmethod
-    def exit(John, event):
-        pass
-
-    @staticmethod
-    def do(John):
-        John.frame = (John.frame + 1) % 8
-        John.timer -= 1
-        John.x += John.velocity
-        John.x = clamp(25, John.x, 1600 - 25)
-        John.y += John.jump
-        John.y = clamp(90, John.y, 800)
-        John.jump -= gravity
-        pass
-
-    @staticmethod
-    def draw(John):
-        if John.velocity == 1:
-            John.image.clip_draw(John.frame * 100, 100, 100, 100, John.x, John.y)
-        else:
-            John.image.clip_draw(John.frame * 100, 0, 100, 100, John.x, John.y)
-        pass
+            John.image.clip_draw(int(John.frame) * 100, 0, 100, 100, John.x, John.y)
 
 
 next_state_table = {
-    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, UP_DOWN: JumpState,
-                IDLE_UP: IdleState},
-    RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
-               UP_DOWN: JumpState, RUN_UP: RunState},
-    JumpState: {RUN_UP: RunState, IDLE_UP: IdleState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState}
+    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, UP_DOWN: IdleState,
+                UP_UP: IdleState},
+    RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState, UP_DOWN: RunState,
+               UP_UP: RunState}
 }
+
 
 class John:
 
     def __init__(self):
-        self.x, self.y = 1600 // 2, 90
+        self.x, self.y = 1920 // 2, 90
+        self.y2 = 90
         self.image = load_image('animation_sheet.png')
         self.dir = 1
         self.velocity = 0
         self.frame = 0
         self.timer = 0
         self.jump = 0
+        self.jump_toggle = True
         self.event_que = []
         self.cur_state = IdleState
         self.cur_state.enter(self, None)
